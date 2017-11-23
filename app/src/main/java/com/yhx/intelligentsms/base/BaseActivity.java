22cd14385390779a982379a5cd482a6922592197
sunshineCEO <1298435317@qq.com>
2017-11-24 00:51:41 +0800

    添加智慧短信项目，定义选项卡布局

---
+++ app/src/main/java/com/yhx/intelligentsms/base/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.yhx.intelligentsms.base;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/11/24.
+ */
+
+public abstract class BaseActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+        initListener();
+        initData();
+    }
+
+    public abstract void initView();
+    public abstract void initListener();
+    public abstract void initData();
+    public abstract void processClick(View v);
+
+    @Override
+    public void onClick(View v) {
+        processClick(v);
+    }
+}
---
+++ app/src/main/java/com/yhx/intelligentsms/ui/activity/MainActivity.java
@@ -0,0 +1,32 @@
+package com.yhx.intelligentsms.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.yhx.intelligentsms.R;
+import com.yhx.intelligentsms.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public void initView() {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void processClick(View v) {
+
+    }
+}
